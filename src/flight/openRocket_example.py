51f9fc89a520c294b4010dd5461bfe5e4f68c031
Ethan Rajah <79022687+EthanRajah@users.noreply.github.com>
2021-06-04 15:53:19 -0400

    Updated openRocket_example.py
    kinematics + dynamics via OR

--- src/flight/openRocket_example.py
+++ src/flight/openRocket_example.py
@@ -1,25 +1,49 @@
-import os
+"""
+*** Refer to the openRocket_flight documentation for a more elaborate description of the code's use ***
 
-import numpy as np
-from matplotlib import pyplot as plt
+- This file uses openRocket_flight.py to run an example simulation from the provided data within OpenRocket.
 
-import orhelper
-from orhelper import FlightDataType, FlightEvent
+- Ensure that the .ork and .rse files (in this case, Houbolt_Jr.ork and utat_test.rse) are located within the
+  'Simulation' folder. The Simulation folder must be located in the same directory as the code.
+
+- When the simulation has been successfully completed, you will see the message,'Simulation finished'.
+
+- When the variables have been successfully classified and organized, you will see the message,'Variables prepared'.
+
+"""
+
+'''Running the simulation'''
 
 from openRocket_flight import *
 
+flight_run = openRocket_flight("Houbolt_Jr.ork", "utat_test.rse")  # Input .ork and .rse files used for the simulation
+flight_run.run()  # This command will run the simulation through the openRocket_flight.py file.
 
+'''To access the variables received from the simulation, use the following format: flight_run.UTAT_family.TYPE'''
 
-flight_run = openRocket_flight("Houbolt_Jr.ork","utat_test.rse")
-flight_run.run()
+print(flight_run.kinematics_dynamics.TYPE_AOA)
+print(flight_run.rocket_properties.TYPE_REFERENCE_LENGTH)
+
+'''Additional variables that can be accessed within the code. Refer to the code documentation for variable descriptions'''
+
+state_vector = flight_run.state_vector
+euler_axis = flight_run.euler_axis
+quaternion = flight_run.quaternion
+recovery_time = flight_run.recovery_time
+
+'''Example 1: Using the simulation variables from the openRocket_flight.py file to generate a 2D graph'''
+
 plt.plot(flight_run.simulation_information.TYPE_TIME, flight_run.kinematics_dynamics.TYPE_ORIENTATION_THETA)
 plt.show()
 
-ax = plt.axes(projection = '3d')
+'''Example 2: Using the simulation variables from the openRocket_flight.py file to generate a 3D graph'''
+
+ax = plt.axes(projection='3d')
 x = flight_run.kinematics_dynamics.TYPE_POSITION_X
 y = flight_run.kinematics_dynamics.TYPE_POSITION_Y
 z = flight_run.kinematics_dynamics.TYPE_ALTITUDE
-ax.plot(x,y,z)
+
+ax.plot(x, y, z)
 ax.set_xlim3d([-1000, 1000])
 ax.set_ylim3d([-1000, 1000])
 ax.set_zlim3d([0, 3000])
