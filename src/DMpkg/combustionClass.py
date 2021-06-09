9dbdaa91ce19f0180ec7ac3bdecf846fb6382279
Riddhiman Roy <riddhiman.roy2015@gmail.com>
2021-06-08 23:17:52 -0400

    Added cea.py
    Added for linking

--- src/DMpkg/combustionClass.py
+++ src/DMpkg/combustionClass.py
@@ -5,13 +5,28 @@ import numpy as np
 class combustionClass:
     def __init__(self, input) -> None:
         self.input = houbolt_jr_single()
-        self.util = utilitiesClass(self.input)
+        self.util = rocket.utilitiesClass(self.input)
 
     def getPropellantTags(self):
         self.fuelTag = self.input.get("fuel")
         self.oxidizerTag = self.input.get("ox")
 
-    
+    def get_CEA(self):
+        
+        houbolt_jr                     = rocket.cea() #are these meant to be default values? where to get params from?
+        self.output.gamm_e             = houbolt_jr.exit_MolWt_gamma
+        self.output.rho_e              = houbolt_jr.MachNumber
+        self.output.Isp                = houbolt_jr.Isp
+        self.output.Te                 = houbolt_jr.T_e
+        self.output.Pe                 = houbolt_jr.P_e
+        self.output.Re                 = self.output.Pe / (self.output.rho_e * self.output.Te)
+        self.output.gamm               = houbolt_jr.Chamber_MolWt_gamma
+        self.output.cstar              = houbolt_jr.Cstar
+        self.output.Tcc                = houbolt_jr.temperatures[0]
+
+        
+
+
 
 
 
