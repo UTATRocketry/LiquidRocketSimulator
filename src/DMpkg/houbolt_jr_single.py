81b663c06f823e12b1845e562a9d2e334bd45913
Akhil Ambardekar <42975101+AkhilAmbardekar@users.noreply.github.com>
2021-06-18 20:43:11 -0400

    json -> yaml + comments

--- src/DMpkg/houbolt_jr_single.py
+++ src/DMpkg/houbolt_jr_single.py
@@ -1,12 +1,12 @@
 import math
-import json
+import yaml
 import DMpkg as rocket
 from DMpkg.utilitiesClass import cellss
 
 def houbolt_jr_single(inp_path):
 
     with open(inp_path, "r") as f:
-            inp_dic = json.load(f)
+            inp_dic = yaml.load(f)
 
     # engine_dic = inp_dic.get("engine")
     # settings_dic = inp_dic.get("settings")
@@ -19,18 +19,22 @@ def houbolt_jr_single(inp_path):
     # oxPres_dic = inp_dic.get("oxPres")
 
     # inp_dic["mass"]["data"] = get_mass_budget(inp_dic.get("mass").get("data"))
+
+    """
+    conversions and calculations that cannot be handled in the input.yaml file
+    """
     
-    inp_dic["fPres"]["Pinit"] = 3500 * inp_dic.get("settings").get("cnv")
+    inp_dic["fPres"]["Pinit"] = inp_dic.get("fPres").get("Pinit") * inp_dic.get("settings").get("cnv") # converting from psi to Pa
     inp_dic["fPres"]["mInit"] = inp_dic.get("fPres").get("vTank") * inp_dic.get("fPres").get("Rhoinit")
 
-    inp_dic["fuel"]["Pinit"] = 525 * inp_dic.get("settings").get("cnv")
-    inp_dic["fuel"]["vTank"] = inp_dic.get("fuel").get("mInit") * (1 + inp_dic.get("fuel").get("ullage")) / inp_dic.get("fuel").get("Rhoinit")
+    inp_dic["fuel"]["Pinit"] = inp_dic.get("fuel").get("Pinit") * inp_dic.get("settings").get("cnv")
+    inp_dic["fuel"]["vTank"] = inp_dic.get("fuel").get("mInit") * (1 + inp_dic.get("fuel").get("ullage")) / inp_dic.get("fuel").get("Rhoinit") # converting from psi to Pa
     inp_dic["fuel"]["lTank"] = inp_dic.get("fuel").get("vTank") / (math.pi*(0.5*inp_dic.get("design").get("diameter") - input.get("fuel").get("tTank")) ** 2)
 
-    inp_dic["oxPres"]["Pinit"] = 3500 * inp_dic.get("settings").get("cnv")
+    inp_dic["oxPres"]["Pinit"] = inp_dic.get("oxPres").get("Pinit") * inp_dic.get("settings").get("cnv") # converting from psi to Pa
     inp_dic["oxPres"]["mInit"] = inp_dic.get("oxPres").get("vTank") * inp_dic.get("oxPres").get("Rhoinit")
 
-    inp_dic["ox"]["Pinit"] = 525 * inp_dic.get("settings").get("cnv")
+    inp_dic["ox"]["Pinit"] = inp_dic.get("ox").get("Pinit") * inp_dic.get("settings").get("cnv") # converting from psi to Pa
     inp_dic["ox"]["vTank"] = inp_dic.get("ox").get("mInit") * (1 + inp_dic.get("ox").get("ullage")) / inp_dic.get("ox").get("Rhoinit")
     inp_dic["ox"]["lTank"] = inp_dic.get("ox").get("vTank") / (math.pi*(0.5*inp_dic.get("design").get("diameter") - input.get("ox").get("tTank")) ** 2)
 
