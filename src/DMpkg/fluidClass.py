222d880bf65265354da623d8a4579bbc69770ce8
Riddhiman Roy <riddhiman.roy2015@gmail.com>
2021-06-19 20:41:31 -0400

    completed fluidclass

--- src/DMpkg/fluidClass.py
+++ src/DMpkg/fluidClass.py
@@ -5,36 +5,33 @@ from DMpkg.utilitiesClass import cellss
 class fluidClass:
     def __init__(self, input, inputTag) -> None:
 
-        self.phase           = 0
-        if input['fuel']['isPropellant'] == 'true' and input['fuel']['fluidtype']:
-            self.phase       = 1
-            if input['ox']['isPropellant'] == 'true' and input['ox']['fluidtype']:
-                self.phase   = 2
+        self.phase                  = 0   # 1 if pressurant, 2 else
+        if inputTag["name"] == 'N2':
+            self.phase              = 1
+        else:
+            self.phase              = 2
         
-        self.m              = np.zeros((input['sim']['numpt'], self.phase))
-        self.mdot           = np.zeros((input['sim']['numpt'], self.phase))
-        self.n              = np.zeros((input['sim']['numpt'], self.phase))
-        self.P              = np.zeros((input['sim']['numpt'], self.phase))
-        self.T              = np.zeros((input['sim']['numpt'], self.phase))
-        self.l              = np.zeros((input['sim']['numpt'], self.phase))
-        self.cg             = np.zeros((input['sim']['numpt'], self.phase))
-        self.rho            = np.zeros((input['sim']['numpt'], self.phase))
+        self.m                      = np.zeros((input['sim']['numpt'], self.phase))
+        self.mdot                   = np.zeros((input['sim']['numpt'], self.phase))
+        self.n                      = np.zeros((input['sim']['numpt'], self.phase))
+        self.P                      = np.zeros((input['sim']['numpt'], self.phase))
+        self.T                      = np.zeros((input['sim']['numpt'], self.phase))
+        self.l                      = np.zeros((input['sim']['numpt'], self.phase))
+        self.cg                     = np.zeros((input['sim']['numpt'], self.phase))
+        self.rho                    = np.zeros((input['sim']['numpt'], self.phase))
 
-        self.MW             = inputTag['MW'] #in the input file there's 4 of them; do these calculations for each?
-        self.rho            = 8314/self.MW
-        self.name           = inputTag['name']
+        self.MW                     = inputTag['MW']
+        self.rho                    = 8314/self.MW
+        self.name                   = inputTag['name']
 
 
-    def setInitialConditions(self, input):
+    def setInitialConditions(self, inputTag):
 
-        ########################################
-        # what do I do with this section: where does the data come from?
-        # obj.IC.m    = input.m;
-        # obj.IC.T    = input.T;
-        # obj.IC.P    = input.P;
-        # obj.IC.rho  = input.rho;
-        # obj.IC.n    = obj.IC.m/obj.MW;
-        ########################################
+        self.IC.m                   = inputTag["mInit"];
+        self.IC.T                   = inputTag["Tinit"];
+        self.IC.P                   = inputTag["Pinit"];
+        self.IC.rho                 = inputTag["Rhoinit"];
+        self.IC.n                   = self.IC.m/self.MW;
         
         self.initializeMass(self, self.IC.m)
         self.initializeTemperature(self, self.IC.T)
@@ -44,5 +41,30 @@ class fluidClass:
 
 
     def initializeMass(self, initValue):
-        self.m[0: self.phase] = initValue
+        self.m[0]                   = initValue
     
+    def initializeTemperature(self, initValue):
+        self.T[0]                   = initValue
+    
+    def initializePressure(self, initValue):
+        self.P[0]                   = initValue
+    
+    def initializeAmount(self, initValue):
+        self.n[0]                   = initValue
+    
+    def initializeMdot(self, initValue):
+        self.mdot[0]                = initValue
+
+    def getM(self, i):
+        return self.m[i]
+    
+    def getP(self, i):
+        return self.P[i]
+    
+    def getT(self, i):
+        return self.T[i]
+    
+    def getN(self, i):
+        return self.n[i]
+
+        
