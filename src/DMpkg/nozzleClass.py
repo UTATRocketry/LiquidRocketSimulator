bf414a5666ca9e83f7ec3ec1d40c2fa2cc039c17
Riddhiman Roy <riddhiman.roy2015@gmail.com>
2021-06-22 17:45:26 -0400

    Minor fixes

--- src/DMpkg/nozzleClass.py
+++ src/DMpkg/nozzleClass.py
@@ -1,70 +1,71 @@
 import DMpkg as rocket
 import numpy as np
+import math
 class nozzleClass: 
     
     def __init__(self, input = None, utilities = None, propulsion = None, airframe = None) -> None: 
 
-        obj.input       = input
-        obj.util        = utilitiesClass(obj.input)
-        obj.combustion  = combustionClass(obj.input)
+        self.input       = input
+        self.util        = rocket.utilitiesClass(self.input)
+        self.combustion  = rocket.combustionClass(self.input)
 
-        obj.exp         = propertyClass()
+        self.exp         = rocket.propertyClass()
 
-        obj.throat      = propertyClass()
-        obj.throat.r    = []
-        obj.throat.d    = []
-        obj.throat.A    = []
-        obj.throat.T    = []
-        obj.throat.P    = []
+        self.throat      = rocket.propertyClass()
+        self.throat.r    = []
+        self.throat.d    = []
+        self.throat.A    = []
+        self.throat.T    = []
+        self.throat.P    = []
             
-        obj.exit        = propertyClass()
-        obj.exit.r      = []
-        obj.exit.d      = []
-        obj.exit.A      = []
-        obj.exit.T      = []
-        obj.exit.P      = []
+        self.exit        = rocket.propertyClass()
+        self.exit.r      = []
+        self.exit.d      = []
+        self.exit.A      = []
+        self.exit.T      = []
+        self.exit.P      = []
 
-        obj.designVars  = propertyClass()
+        self.designVars  = rocket.propertyClass()
 
-    def get(obj, alt, g):
+    def get(self, alt, g):
             
-        atmos                   = obj.util.stdAtmos(alt * obj.input.settings.optAltFac)
+        atmos                   = self.util.stdAtmos(alt * self.input.settings.optAltFac)
 
-        obj.expansionRatio(g, atmos.P, obj.designVars.Pcc * obj.input.settings.cnv)
-        comb                    = obj.combustion.get(obj.designVars.OF,   \
-                                                        obj.designVars.Pcc,  \
-                                                        obj.exp) #this will need to be changed if combustion.get changes
-        obj.areas(comb)
-        obj.dimensions()
+        self.expansionRatio(g, atmos.P, self.designVars.Pcc * self.input.settings.cnv)
+        comb                    = self.combustion.get(self.designVars.OF,   \
+                                                        self.designVars.Pcc,  \
+                                                        self.exp) #this will need to be changed if combustion.get changes
+        self.areas(comb)
+        self.dimensions()
     
 
-    def areas(obj, comb):
+    def areas(self, comb):
 
-        Pcc                     = obj.designVars.Pcc * obj.input.settings.cnv
-        mdot                    = obj.designVars.mDotox * (1 + 1/obj.designVars.OF)
+        Pcc                     = self.designVars.Pcc * self.input.settings.cnv
+        mdot                    = self.designVars.mDotox * (1 + 1/self.designVars.OF)
 
 
-        A                       = sqrt(comb.Tcc)/Pcc
-        B                       = sqrt(comb.Re/comb.gamm)
+        A                       = math.sqrt(comb.Tcc)/Pcc
+        B                       = math.sqrt(comb.Re/comb.gamm)
         C                       = (comb.gamm+1)/(2*(comb.gamm-1))
 
-        obj.throat.A            = mdot*A*B*((comb.gamm+1)/2)^C
-        obj.exit.A              = obj.throat.A * obj.exp
+        self.throat.A            = mdot*A*B*((comb.gamm+1)/2)^C
+        self.exit.A              = self.throat.A * self.exp
     
         
-    def dimensions(obj):
-        obj.throat.r            = sqrt(obj.throat.A / pi)
-        obj.throat.d            = obj.throat.r * 2
+    def dimensions(self):
+        self.throat.r            = math.sqrt(self.throat.A / math.pi)
+        self.throat.d            = self.throat.r * 2
             
-        obj.exit.r              = sqrt(obj.exit.A / pi)
-        obj.exit.d              = obj.exit.r * 2            
+        self.exit.r              = math.sqrt(self.exit.A / math.pi)
+        self.exit.d              = self.exit.r * 2            
     
 
-    def expansionRatio(obj, gamma, P_e, P_0):
+    def expansionRatio(self, gamma, P_e, P_0):
 
         A                       = ((gamma+1)/2)^(1/(gamma-1))
         B                       = (P_e/P_0)^(1/gamma)
         C                       = ((gamma + 1)/(gamma - 1))
         D                       = 1-(P_e/P_0)^((gamma - 1)/gamma)
 
-        obj.exp                 = 1/(A*B*sqrt(C*D))
+        self.exp                 = 1/(A*B*math.sqrt(C*D))
